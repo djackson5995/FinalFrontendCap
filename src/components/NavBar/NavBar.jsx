93a1fd5267442940259160e69bcfb19e17855b71
djackson5995 <146674106+djackson5995@users.noreply.github.com>
2024-01-28 21:37:57 -0700

    first commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+
+const NavBar = () => {
+  return <Stack></Stack>;
+};
+
+export default NavBar;
